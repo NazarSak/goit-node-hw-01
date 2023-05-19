1479a6d2f83f0b3f74a85841bc4269207009598c
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-05-19 22:45:26 +0300

    update

--- project-node/db/contacts.js
+++ project-node/db/contacts.js
@@ -22,12 +22,24 @@ const addContact = async (data) => {
     ...data,
   };
   contacts.push(newContact);
-  await fs.writeFile(contactPath, JSON.stringify(contacts,null,2));
-  return newContact
+  await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((contact) => contact.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactPath, JSON.stringify(contacts, null, 2));
+  return result;
 };
 
 module.exports = {
   listContacts,
   getContactById,
   addContact,
+  removeContact,
 };
--- project-node/index.js
+++ project-node/index.js
@@ -1,6 +1,16 @@
 const contact = require("./db/contacts");
+const { Command } = require("commander");
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
 
-console.log(__dirname);
+  program.parse(process.argv);
+  const argv = program.opts();
+// console.log(__dirname);
 
 const contactAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
@@ -13,19 +23,27 @@ const contactAction = async ({ action, id, name, email, phone }) => {
       return console.log(ByIdContact);
 
     case "add":
-      const newContact = await contact.addContact({name, email, phone});
+      const newContact = await contact.addContact({ name, email, phone });
       return console.log(newContact);
+
+    case "remove":
+      const deleteContact = await contact.removeContact(id);
+      return console.log(deleteContact);
 
     default:
       console.warn("\x1B[31m Unknown action type!");
   }
 };
 
+
+contactAction(argv)
+
 // contactAction({ action: "list" });
 // contactAction({ action: "get", id: "05olLMgyVQdWRwgKfg5J6" });
-contactAction({
-  action:"add",
-  name: "Nazar",
-  email: "nazar.ante@vestibul.co.uk",
-  phone: "(414) 820-6885",
-});
+// contactAction({
+//   action:"add",
+//   name: "Nazar",
+//   email: "nazar.ante@vestibul.co.uk",
+//   phone: "(414) 820-6885",
+// });
+// contactAction({ action: "remove", id: "rsKkOQUi80UsgVPCcLZZW" });
